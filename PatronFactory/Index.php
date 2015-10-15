33288f006e26fe139b93a160da1e13c29c883df6
Burris19 <julianhernandez19@gmail.com>
2015-10-15 17:16:12 -0600

    [update] separando codigo

--- PatronFactory/Index.php
+++ PatronFactory/Index.php
@@ -6,11 +6,27 @@ $_SESSION['user']['name'] = 'Juliancito';
 require ('User.php');
 require ('Coffee.php');
 
+class AuthenticationService {
+
+    public function user()
+    {
+        $user = new User($_SESSION['user']);
+    }
+
+}
+
 class Controller {
+
+    private $auth;
+
+    public function __construct(AuthenticationService $auth)
+    {
+        $this->auth = $auth;
+    }
 
     public function action()
     {
-        $user = new User($_SESSION['user']);
+        $user = $this->auth->user();
         $coffee = new Coffee();
         $menssage = $user->drink($coffee);
 
@@ -19,7 +35,10 @@ class Controller {
 
 
 }
+/*
 
-$controller = new Controller;
-
+$controller = new Controller(new AuthenticationService $user );
 $controller->action();
+*/
+
+$user = new AuthenticationService
